14481a2fcde635d6dcfaf8d098361d11ff200e62
mateusviana743 <mateusviana093@gmail.com>
2025-07-14 21:31:48 -0300

    Update testarcotacao.js

--- src/testarcotacao.js
+++ src/testarcotacao.js
@@ -1,6 +1,11 @@
 const { salvarCotacao } = require('./services/salvarcotacaoservice');
 
 (async () => {
+  try {
+    await salvarCotacao('ITSA4');  
+    await salvarCotacao('PETR4');
     await salvarCotacao('BOVA11');
-  })();
-  
+  } catch (err) {
+    console.error('Erro ao executar salvarCotacao:', err.message);
+  }
+})();
